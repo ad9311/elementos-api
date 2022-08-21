a3d86f2ead9601cb853adc43495d5115fcd400cf
Ángel Díaz <diazangel9311@gmail.com>
2022-08-20 20:50:54 -0500

    WIP - Implement element model struct

--- internal/db/model.go
+++ internal/db/model.go
@@ -4,23 +4,32 @@ import "time"
 
 // User is the users database model.
 type User struct {
-	ID                int64     `json:"id"`
-	FirstName         string    `json:"first_name"`
-	LastName          string    `json:"last_name"`
-	Username          string    `json:"username"`
-	Email             string    `json:"email"`
-	EncryptedPassword string    `json:"password"`
-	DefaultUser       bool      `json:"default_user"`
-	LastLogin         time.Time `json:"last_login"`
-	CreatedAt         time.Time `json:"created_at"`
-	UpdatedAt         time.Time `json:"updated_at"`
+	ID                int64
+	FirstName         string
+	LastName          string
+	Username          string
+	Email             string
+	EncryptedPassword string
+	DefaultUser       bool
+	LastLogin         time.Time
+	CreatedAt         time.Time
+	UpdatedAt         time.Time
 }
 
 // InviationCodes is the code used for sigining up.
 type InviationCodes struct {
-	ID        int64     `json:"id"`
-	Code      string    `json:"code"`
-	Validity  time.Time `json:"validity"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	ID        int64
+	Code      string
+	Validity  time.Time
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+// Element is the periodic table element database model.
+type Element struct {
+	ID           int64
+	Name         string
+	Symbol       string
+	AtomicNumber string
+	AtomicMass   string
 }
