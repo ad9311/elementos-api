ceb81614657cec48e7af6be84df7a9147117f8ea
Ángel Díaz <diazangel9311@gmail.com>
2022-08-22 14:28:44 -0500

    Refactor views (#33)
    * Implement render and session package
    
    * WIP - refactor server package
    
    * Refacto server.view_handler.go to controller package
    
    * Remove unnecessary variables from server
    
    Refactor server to only require a session and a configuration
    
    * Implement sess.app instance functions as helpers
    
    * Refactor web/templates folder
    
    Partials are now in their related folders with the views
    Save partials with unique ids in the templates map
    
    * Refactor user's session registration form

--- internal/controller/landmarks_controller.go
+++ internal/controller/landmarks_controller.go
@@ -12,10 +12,11 @@ import (
 func GetDashboard(w http.ResponseWriter, r *http.Request) {
 	if App.IsUserSignedIn(r) {
 		App.CSRFToken = nosurf.Token(r)
-		if err := render.WriteView(w, "dashboard.view.html"); err != nil {
+		App.URL = r.URL.String()
+		if err := render.WriteView(w, "landmarks_index"); err != nil {
 			fmt.Println(err)
 		}
 	} else {
-		http.Redirect(w, r, "sign_in", http.StatusSeeOther)
+		http.Redirect(w, r, "/sign_in", http.StatusSeeOther)
 	}
 }
