87c4b6a6c1376c8f62fd45ac96de3ab0a4d85d51
Ángel Díaz <diazangel9311@gmail.com>
2022-08-26 00:50:47 -0500

    Implement basic structure
    Implement sign-in form

---
+++ internal/db/model.go
@@ -0,0 +1,33 @@
+package db
+
+import "time"
+
+// User ...
+type User struct {
+	ID        int64
+	FirstName string
+	LastName  string
+	Username  string
+	Default   bool
+	LastLogin time.Time
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+// Landmark ...
+type Landmark struct {
+	ID        int64
+	UserID    int64
+	Default   bool
+	CreatedBy string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+
+	Name        string   `json:"name"`
+	NativeName  string   `json:"nativeName"`
+	Category    string   `json:"category"`
+	Description string   `json:"description"`
+	WikiURL     string   `json:"wikiURL"`
+	Location    []string `json:"location"`
+	ImgURLs     []string `json:"imgURL"`
+}
---
+++ internal/server/router.go
@@ -0,0 +1,50 @@
+package server
+
+import (
+	"net/http"
+
+	"github.com/ad9311/hitomgr/internal/ctrl"
+	"github.com/go-chi/chi"
+)
+
+func routes() http.Handler {
+	mux := chi.NewRouter()
+
+	// mux.Use(middleware.Recoverer)
+	mux.Use(loadSession)
+	mux.Use(newCSRF)
+
+	mux.Get("/sign_in", ctrl.GetSignIn)
+
+	// // Root
+	// mux.Get("/", controller.GetRoot)
+
+	// // Users
+	// mux.Route("/sign_in", func(r chi.Router) {
+	// 	r.Get("/", controller.GetSignIn)
+	// 	r.Post("/", controller.PostSignIn)
+	// })
+	// mux.Route("/sign_up", func(r chi.Router) {
+	// 	r.Get("/", controller.GetSignUp)
+	// 	r.Post("/", controller.PostSignUp)
+	// })
+	// mux.Post("/sign_out", controller.PostSignOut)
+
+	// // Landmarks
+	// mux.Get("/dashboard", controller.GetDashboard)
+	// mux.Route("/landmarks", func(r chi.Router) {
+	// 	r.Post("/", controller.PostNewLandmark)
+	// 	r.Get("/new", controller.GetNewLandmark)
+	// 	r.Route("/{landmarkID}", func(r chi.Router) {
+	// 		r.Get("/", controller.GetShowLandmark)
+	// 		r.Get("/edit", controller.GetEditLandmark)
+	// 		r.Post("/", controller.PostEditLandmark)
+	// 		r.Post("/delete", controller.PostDeleteLandmark)
+	// 	})
+	// })
+
+	fileServer := http.FileServer(http.Dir("./web/static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static/", fileServer))
+
+	return mux
+}
