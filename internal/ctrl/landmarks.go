1eef4213de65577b52db46c5d06c9ec945aaad1b
Ángel Díaz <diazangel9311@gmail.com>
2022-08-26 19:37:31 -0500

    Implement create new landmark

--- internal/ctrl/landmarks.go
+++ internal/ctrl/landmarks.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/ad9311/hitomgr/internal/render"
+	"github.com/ad9311/hitomgr/internal/val"
 	"github.com/justinas/nosurf"
 )
 
@@ -15,6 +16,50 @@ func GetDashboard(w http.ResponseWriter, r *http.Request) {
 		appMap["CSRFToken"] = nosurf.Token(r)
 		appMap["CurrentUser"] = currentUser(r)
 		if err := render.WriteView(w, "landmarks_index", appMap); err != nil {
+			fmt.Println(err)
+		}
+	} else {
+		http.Redirect(w, r, "/sign_in", http.StatusSeeOther)
+	}
+}
+
+// GetNewLandmark ...
+func GetNewLandmark(w http.ResponseWriter, r *http.Request) {
+	if session.GetBool(r.Context(), "user_signed_in") {
+		appMap := make(map[string]interface{})
+		appMap["CSRFToken"] = nosurf.Token(r)
+		appMap["CurrentUser"] = currentUser(r)
+		if err := render.WriteView(w, "landmarks_new", appMap); err != nil {
+			fmt.Println(err)
+		}
+	} else {
+		http.Redirect(w, r, "/sign_in", http.StatusSeeOther)
+	}
+}
+
+// PostNewLandmark ...
+func PostNewLandmark(w http.ResponseWriter, r *http.Request) {
+	lm, err := val.ValidateNewLandmark(database, r)
+	if err != nil {
+		fmt.Println(err)
+		http.Redirect(w, r, "/landmarks/new", http.StatusSeeOther)
+	} else {
+		path := fmt.Sprintf("/landmarks/%d", lm.ID)
+		http.Redirect(w, r, path, http.StatusSeeOther)
+	}
+}
+
+// GetShowLandmark ...
+func GetShowLandmark(w http.ResponseWriter, r *http.Request) {
+	if session.GetBool(r.Context(), "user_signed_in") {
+		lm, err := val.ValidateShowLandmark(database, r.URL.String())
+		if err != nil {
+			fmt.Println(err)
+			http.Redirect(w, r, "/dashboard", http.StatusSeeOther)
+		}
+		appMap := make(map[string]interface{})
+		appMap["Landmark"] = lm
+		if err := render.WriteView(w, "landmarks_show", appMap); err != nil {
 			fmt.Println(err)
 		}
 	} else {
---
+++ internal/db/landmarks.go
@@ -0,0 +1,111 @@
+package db
+
+import (
+	"context"
+	"time"
+)
+
+// SelectLandmarkByID ...
+func (d *Database) SelectLandmarkByID(id int64) (Landmark, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	landmark := Landmark{}
+	location := ""
+	imgURLs := ""
+	query := `SELECT landmarks.*,users.username
+	FROM landmarks RIGHT JOIN users ON users.id=landmarks.user_id WHERE landmarks.id=$1`
+
+	row := d.Conn.QueryRowContext(ctx, query, id)
+	err := row.Scan(
+		&landmark.ID,
+		&landmark.Name,
+		&landmark.NativeName,
+		&landmark.Category,
+		&landmark.Description,
+		&landmark.WikiURL,
+		&location,
+		&imgURLs,
+		&landmark.Default,
+		&landmark.UserID,
+		&landmark.CreatedAt,
+		&landmark.UpdatedAt,
+		&landmark.CreatedBy,
+	)
+	if err != nil {
+		return landmark, err
+	}
+
+	landmark.Location = pgArrayToSlice(location)
+	landmark.ImgURLs = pgArrayToSlice(imgURLs)
+
+	return landmark, nil
+}
+
+// SelectLandmarkByName ...
+func (d *Database) SelectLandmarkByName(name string) (Landmark, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	landmark := Landmark{}
+	location := ""
+	imgURLs := ""
+	query := `SELECT landmarks.*,users.username
+	FROM landmarks RIGHT JOIN users ON users.id=landmarks.user_id WHERE landmarks.name=$1`
+
+	row := d.Conn.QueryRowContext(ctx, query, name)
+	err := row.Scan(
+		&landmark.ID,
+		&landmark.Name,
+		&landmark.NativeName,
+		&landmark.Category,
+		&landmark.Description,
+		&landmark.WikiURL,
+		&location,
+		&imgURLs,
+		&landmark.Default,
+		&landmark.UserID,
+		&landmark.CreatedAt,
+		&landmark.UpdatedAt,
+		&landmark.CreatedBy,
+	)
+	if err != nil {
+		return landmark, err
+	}
+
+	landmark.Location = pgArrayToSlice(location)
+	landmark.ImgURLs = pgArrayToSlice(imgURLs)
+
+	return landmark, nil
+}
+
+// InsertLandmark ...
+func (d *Database) InsertLandmark(formMap map[string]string) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	query := `INSERT INTO landmarks
+	(name,native_name,category,description,wiki_url,
+	location,img_urls,user_id,created_at,updated_at)
+	values ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10);
+	`
+	_, err := d.Conn.ExecContext(
+		ctx,
+		query,
+		formMap["name"],
+		formMap["native_name"],
+		formMap["category"],
+		formMap["description"],
+		formMap["wiki_url"],
+		formMap["location"],
+		formMap["img_urls"],
+		formMap["user_id"],
+		time.Now(),
+		time.Now(),
+	)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
