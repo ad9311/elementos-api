cac18ff207bc0bcd5c3e64faee58210d7814c444
Ángel Díaz <diazangel9311@gmail.com>
2022-08-30 23:15:05 -0500

    Implement notice and alerts div in navbar
    Implement errs for error constants

---
+++ internal/errs/errs.go
@@ -0,0 +1,10 @@
+package errs
+
+const (
+	PswdConfMismatch   = "password confirmation mismatches"
+	PswdsOrUsernameErr = "incorrect password or username"
+	MissingFormField   = "form is missing %s field"
+	EmptyFormField     = "form %s field is empty"
+	ExpiredDate        = "%s date has expired"
+	FormErr            = "form error"
+)
